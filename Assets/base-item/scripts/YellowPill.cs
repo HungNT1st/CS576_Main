6a80215ca3a9692deb2447a7707a138a2e810353
TomNgo03 <ngoquangtung281203@gmail.com>
2024-12-15 17:54:20 -0500

    update script for item

--- Assets/base-item/scripts/YellowPill.cs
+++ Assets/base-item/scripts/YellowPill.cs
@@ -1,32 +1,39 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class YellowPill : BasePill
-// {
-//     [SerializeField] private float speedMultiplier = 0.5f;
+public class YellowPill : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier = 0.5f;
+    [SerializeField] private float duration = 5f;
 
-//     private void Awake()
-//     {
-//         duration = 5f;
-//     }
+    private void OnTriggerEnter(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+        if (player != null)
+        {
+            // Store original speeds
+            float originalWalkSpeed = player.walkSpeed;
+            float originalSprintSpeed = player.sprintSpeed;
 
-//     protected override void ActivateEffect()
-//     {
-//         base.ActivateEffect();
+            // Apply speed reduction
+            player.walkSpeed *= speedMultiplier;
+            player.sprintSpeed *= speedMultiplier;
+
+            // Reset after duration
+            StartCoroutine(ResetSpeedAfterDelay(player, originalWalkSpeed, originalSprintSpeed));
+
+            // Destroy the pill
+            Destroy(gameObject);
+        }
+    }
+
+    private System.Collections.IEnumerator ResetSpeedAfterDelay(FirstPersonController player, float originalWalkSpeed, float originalSprintSpeed)
+    {
+        yield return new WaitForSeconds(duration);
         
-//         VillainBehavior villain = GameObject.FindGameObjectWithTag("Player").GetComponent<VillainBehavior>();
-//         if (villain != null)
-//         {
-//             villain.SetSpeedMultiplier(speedMultiplier);
-//         }
-//     }
-
-//     protected override void DeactivateEffect()
-//     {
-//         VillainBehavior villain = GameObject.FindGameObjectWithTag("Player").GetComponent<VillainBehavior>();
-//         if (villain != null)
-//         {
-//             villain.ResetSpeed();
-//         }
-//         base.DeactivateEffect();
-//     }
-// } 
+        if (player != null)
+        {
+            player.walkSpeed = originalWalkSpeed;
+            player.sprintSpeed = originalSprintSpeed;
+        }
+    }
+} 
