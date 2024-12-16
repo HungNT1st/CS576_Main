f8ba1d4feb346ff548d34018e3bf85197b58784a
TungVLe <tunle0801@gmail.com>
2024-12-16 12:29:31 -0500

    fix bug

--- Assets/Garbage/Trash.cs
+++ Assets/Garbage/Trash.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Trash : MonoBehaviour
 {
-    [SerializeField] private CoinManager coinManager;
     private void Start() {
         GetComponent<Collider>().isTrigger = true;
     }
@@ -14,10 +13,8 @@ public class Trash : MonoBehaviour
             HUD.Instance.SetSmallTaskLoading("Cleaning up trash", 4).onComplete += () => {
                 Destroy(gameObject);
                 GameManager.Instance.HealWorld(5);
-                if (coinManager != null) {
-                    coinManager.AddCoins(1);
-                    HUD.Instance.PopUpText("Trash collected successfully!", 2);
-                }
+                CoinManager.Instance.AddCoins(1);
+                HUD.Instance.PopUpText("Trash collected successfully!", 2);
             };
         }
     }
